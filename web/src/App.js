00cdaad4a35572b5fe6194d41f71e7ed5aa941f5
Sean McIntyre <boxysean@gmail.com>
2017-10-29 21:01:55 -0400

    Do dat ding

--- web/src/App.js
+++ web/src/App.js
@@ -5,7 +5,6 @@ import {
     Link,
 } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 
--- web/src/pages/game/index.js
+++ web/src/pages/game/index.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: "off" */
+
 import React from 'react';
 
 import { Form, FormGroup, ControlLabel, FormControl, HelpBlock, Button, Checkbox, Col, Jumbotron } from 'react-bootstrap';
