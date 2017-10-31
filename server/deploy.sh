23012489f1e442e3f8aa6d1bb5756c90ec730511
Sean McIntyre <boxysean@gmail.com>
2017-10-30 23:34:31 -0400

    Almost there

--- server/deploy.sh
+++ server/deploy.sh
@@ -10,5 +10,11 @@ export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
 
-zappa deploy dev || echo "No deploy needed"
-zappa update dev
+# remove profile_name from zappa_settings.json
+
+jq -a 'del(.dev.profile_name)' zappa_settings.json > zappa_settings.json.tmp
+
+zappa deploy -s zappa_settings.json.tmp dev || echo "No deploy needed"
+zappa update -s zappa_settings.json.tmp dev
+
+rm zappa_settings.json.tmp
