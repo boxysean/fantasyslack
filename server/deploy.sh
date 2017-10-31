a866f92c4e654dfec847f2ede7c1dffef2ec230a
Sean McIntyre <boxysean@gmail.com>
2017-10-30 22:58:36 -0400

    Let's try again

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-zappa_role_file="assume-role-output.txt"
-
-(aws sts assume-role \
+temp_role=$(aws sts assume-role \
   --role-arn arn:aws:iam::858316752957:role/server-dev-ZappaLambdaExecutionRole \
-  --role-session-name "DeployZappa" > $zappa_role_file \
-  && source $zappa_role_file && rm $zappa_role_file) || echo skipping assume role...
+  --role-session-name "DeployZappa")
+
+echo "Became role/server-dev-ZappaLambdaExecutionRole"
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
 
 zappa update dev
