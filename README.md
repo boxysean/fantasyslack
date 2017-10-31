b803618401d05a0d11efe3c4331bf4755d1a93c7
Sean McIntyre <boxysean@gmail.com>
2017-10-31 01:33:03 -0400

    shruggie

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
+Test: https://s3.amazonaws.com/fantasyslack/index.html
 
-a react front-end <-> an API <-> a slackbot
-
-Tester: https://s3.amazonaws.com/fantasyslack/index.html
 Auto-built with any push to this repo
--- server/deploy.sh
+++ server/deploy.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 temp_role=$(aws sts assume-role \
-  --role-arn arn:aws:iam::858316752957:role/server-dev-ZappaLambdaExecutionRole \
-  --role-session-name "DeployZappa")
+  --role-arn arn:aws:iam::858316752957:role/FantasySlackDeployZappa \
+  --role-session-name "FantasySlackDeployZappa")
 
-echo "Became role/server-dev-ZappaLambdaExecutionRole"
+echo "Became role/FantasySlackDeployZappa"
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
