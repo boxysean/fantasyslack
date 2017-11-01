53da147faa73c8dc8d6b278f332de09c8bfb26e9
Sean McIntyre <boxysean@gmail.com>
2017-10-31 21:24:33 -0400

    Flask routes

--- server/fantasyslack/app.py
+++ server/fantasyslack/app.py
@@ -1,7 +1,8 @@
 import collections
+import json
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -14,20 +15,31 @@ def index():
 @app.route('/api/v1/slack-event', methods=['POST'])
 def slack_event():
     if request.json['type'] == 'url_verification':
-        if request.json['token'] != config.slack_token:
+        if request.json['token'] != config['slack_token']:
             return 'Forbidden', 403
 
-        return {
+        return jsonify({
             'challenge': request.json['challenge'], 
-        }, 200
+        })
 
 
 try:
-    from fantasyslack.secrets import secrets
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(
+        Bucket='fantasyslack-conf',
+        Key='secrets.json',
+    )
+    remote_secrets = json.load(response['Body'].read())
 except:
-    secrets = {}
+    remote_secrets = {}
 
-config = collections.ChainMap(os.environ, secrets)
+try:
+    with open('secrets.json') as secrets_file:
+        local_secrets = json.load(secrets_file)
+except:
+    local_secrets = {}
+
+config = collections.ChainMap(os.environ, remote_secrets, local_secrets)
 
 
 if __name__ == '__main__':
