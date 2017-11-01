002fb271857a7c09c88d6386feb0586706391694
Sean McIntyre <boxysean@gmail.com>
2017-10-31 20:51:28 -0400

    Add verification route

--- server/fantasyslack/app.py
+++ server/fantasyslack/app.py
@@ -1,15 +1,34 @@
-from flask import Flask
+import collections
+import os
+
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/')  
 def index():  
     return "Hello, world (and cats)!", 200
 
-@app.route('/api/v1/slack-event')
-def slack_event():
-    return "What?"
 
-# We only need this for local development.  
-if __name__ == '__main__':  
+@app.route('/api/v1/slack-event', methods=['POST'])
+def slack_event():
+    if request.json['type'] == 'url_verification':
+        if request.json['token'] != config.slack_token:
+            return 'Forbidden', 403
+
+        return {
+            'challenge': request.json['challenge'], 
+        }, 200
+
+
+try:
+    from fantasyslack.secrets import secrets
+except:
+    secrets = {}
+
+config = collections.ChainMap(os.environ, secrets)
+
+
+if __name__ == '__main__':
     app.run()
