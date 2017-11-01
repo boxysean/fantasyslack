f44f6ed6c42421a62692e5733e3b5d0e9be00e31
Sean McIntyre <boxysean@gmail.com>
2017-10-31 20:07:52 -0400

    Add slack route

--- server/fantasyslack/app.py
+++ server/fantasyslack/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():  
     return "Hello, world (and cats)!", 200
 
+@app.route('/api/v1/slack-event')
+def slack_event():
+    return "What?"
+
 # We only need this for local development.  
 if __name__ == '__main__':  
     app.run()
